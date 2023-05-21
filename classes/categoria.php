9d9412f31ee2b4e1ca78d3405abc9424d6d04633
IsraelJulio <83346689+IsraelJulio@users.noreply.github.com>
2023-05-21 21:30:49 +0000

    Adicionadas as classes programa de milhas e categoria

--- classes/categoria.php
+++ classes/categoria.php
@@ -1,1 +1,30 @@
-TODO
+<?php
+declare(strict_types=1);
+include_once('Persiste.php');
+class Categoria extends persist{
+	  private string $nome;
+	  private int $pontuacaoMinima;
+	  static private $filename = 'Categoria.txt';
+	  public function __construct($nome, $pontuacaoMinima){
+		    $this->cliente = $nome;
+		    $this->pontuacaoMinima = $pontuacaoMinima;
+	  }
+	  static public function getFilename(){
+	    	return get_called_class()::$filename;
+	  }
+	  public function getNome(){
+	    	return $this->nome;
+	  }
+	  public function getPontuacaoMinima(){
+	    	return $this->pontuacaoMinima;
+	  }
+  	  public function setNome(string nome){
+	    	$this->nome = nome;
+        $this->nome
+	  }
+	  public function setPontuacaoMinima(int pontuacaoMinima){
+	    	$this->pontuacaoMinima = pontuacaoMinima;
+      return $this->pontuacaoMinima
+	  }
+}?
+?>
